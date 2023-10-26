a6167945c4bd1440356031af3b6c68703fd4e1a5
ZaneleYenziwe <zaneleyenziwe19@gmail.com>
2023-10-25 17:28:56 -0700

    a function that prints a string in reverse

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,20 @@
 /**
  * _puts_recursion -Write a function that prints a string, followed by a new line
  *@s: This is the input string
+ *
  *Return: Always 0
  */
  
 
- void _puts_recursion (char*)
+ void _puts_recursion (char*s)
 
  {
-	 if (*s == 0)
+	 if (*s)
+	 {
+		 _putchar(*s);
+		 s++
+                 _puts_recursion(s);
+	 }
+	 else 
+		 _putchar ('\n');
  }
